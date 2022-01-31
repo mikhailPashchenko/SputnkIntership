e420b375651353aedbfc8ece182f58c02b743671
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-01-31 11:24:55 +0300

    tests for interesting travel

--- YandexContext/F/tests.rb
+++ YandexContext/F/tests.rb
@@ -1,5 +1,13 @@
 require_relative './F_interesting_travel'
 
-puts "test"
+def assert(filename, expected_value)
+  result = Travel::TravelData.new(filename).roads_min_number
+  if result != expected_value
+    puts "error in test #{test}: expected #{expected_value}, got: result"
+  end
+end
 
-Roads::roads_min_number "input.txt"
+assert("input.txt", 3)
+assert("input2.txt", 1)
+assert("input3.txt", -1)
+assert("input4.txt", 2)
