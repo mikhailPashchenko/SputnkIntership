70b65ee259a2052d64966230f54525b99d969ab9
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-04 13:16:51 +0300

    D: recursive algorithm for brakets

--- YandexContext/D/D_brackets_sequence.rb
+++ YandexContext/D/D_brackets_sequence.rb
@@ -1,21 +1,24 @@
-n = File.read("input.txt").to_i
+n = File.read("input.txt").to_i * 2
 
-def brackets(n)
-  result = ["()"]
+sequence_arr = [] # пустой массив, куда кладем скобки
 
-  (n-1).times do
-    temp_result = []
-    result.each do |res|
-      temp_result.push "(#{res})", "#{res}()", "()#{res}"
-    end
-    result = temp_result.uniq
+def generator(left_minus_right, i, n, sequence_arr)
+  # кладем откр. скобку, только если хватит места для (left_minus_right + 1) закрыв. скобок
+  if (left_minus_right <= n - i - 2)
+    sequence_arr[i] = '('
+    generator(left_minus_right + 1, i + 1, n, sequence_arr)
   end
-  result
+  # закр. скобку можно положить всегда, если left_minus_right > 0
+  if left_minus_right > 0
+    sequence_arr[i] = ')'
+    generator(left_minus_right-1, i+1, n, sequence_arr)
+  end
+  # выходим из цикла и печатаем
+  if i == n
+    if left_minus_right == 0
+      puts sequence_arr.join()
+    end
+  end
 end
 
-puts result = brackets(n)
-puts result.size
-puts sorted_result = result.uniq.sort { |a, b| a <=> b }
-puts sorted_result.size
-
-File.write("output.txt", "\nn = #{n}\n" + sorted_result.join("\n"), mode: "a")
+puts generator(0,0,n,sequence_arr)
